04a9cd01aa0bd8ae6c8bf20cd7892c169f66f552
na0chan-go <nn19960218@gmail.com>
2024-08-28 23:28:32 +0900

    feat: task関連を追加

--- main.go
+++ main.go
@@ -16,8 +16,11 @@ func main() {
 		log.Fatalf("Error creating db: %v", err)
 	}
 	ur := repository.NewUserRepository(db)
+	tr := repository.NewTaskRepository(db)
 	uu := usecase.NewUserUsecase(ur)
+	tu := usecase.NewTaskUsecase(tr)
 	uc := controller.NewUserController(uu)
-	e := router.NewRouter(uc)
+	tc := controller.NewTaskController(tu)
+	e := router.NewRouter(uc, tc)
 	e.Logger.Fatal(e.Start(":8080"))
 }
